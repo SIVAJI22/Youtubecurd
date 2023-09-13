e21ea60b8136fb1f784b4e90dbf90ce426d559a9
SIVAJI22 <125645051+SIVAJI22@users.noreply.github.com>
2023-09-13 17:37:01 +0930

    Update videos.js

--- routes/videos.js
+++ routes/videos.js
@@ -22,7 +22,7 @@ res.json({status:e})
 })
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, "../Backend1/frontend2/src/Videos")
+      cb(null, "../Youtubecurd/frontend2/src/Videos")
     },
     filename: function (req, file, cb) {
       const uniqueSuffix = Date.now();
